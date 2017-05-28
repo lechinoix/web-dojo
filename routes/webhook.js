ea0074ac24da10a5d699806840f28f25d716a1ef
Lechinoix <nicolas.ngomai@gmail.com>
2017-05-29 00:21:15 +0200

    Use express router

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,3 +1,14 @@
+var express = require('express');
+var router = express.Router();
 var chatService = require('../server/chatService');
 
-module.exports = chatService.authenticate;
+/* GET hello world page. */
+router.get('/', function(req, res, next) {
+  if(chatService.authenticate(req)) {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+module.exports = router;
